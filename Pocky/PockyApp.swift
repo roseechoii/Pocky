426c15095f84c6bfd4fa5e213ea264783aaa0d43
Rose Choi <rose@Roses-MacBook-Pro.local>
2024-02-22 13:24:16 -0800

    Initial commit

---
+++ Pocky/PockyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PockyApp.swift
+//  Pocky
+//
+//  Created by Rose Choi on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct PockyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
