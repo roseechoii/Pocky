f5344817df7c5ba25d5c057b14bee9a640bca08f
Rose Choi <roseechoii@gmail.com>
2024-02-22 14:46:25 -0800

    Testing commit by adding new file

---
+++ Pocky/MainPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MainPageView.swift
+//  Pocky
+//
+//  Created by Rose Choi on 2/22/24.
+//
+
+import SwiftUI
+
+struct MainPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPageView()
+}
